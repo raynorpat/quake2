06da5c0d525e3a39519b86a38e1c02f5b32add80
Pat Raynor <raynorpat@gmail.com>
2016-06-15 17:04:54 -0400

    engine: remove unused x86.c

--- src/client/client.h
+++ src/client/client.h
@@ -662,14 +662,6 @@ void CL_DrawInventory (void);
 //
 void CL_PredictMovement (void);
 
-#if id386
-void x86_TimerStart( void );
-void x86_TimerStop( void );
-void x86_TimerInit( unsigned long smallest, unsigned longest );
-unsigned long *x86_TimerGetHistogram( void );
-#endif
-
-
 #ifdef QMAX
 void SetParticleImages (void);
 #endif
